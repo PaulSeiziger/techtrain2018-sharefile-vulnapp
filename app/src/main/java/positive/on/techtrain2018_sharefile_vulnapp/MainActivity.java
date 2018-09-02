250efd0b820583ca7d474a39fabdb5a2939eead8
PavelDremanovich <pavel@dremanovich.com>
2018-09-02 16:18:39 +0300

    Fixed

--- app/src/main/java/positive/on/techtrain2018_sharefile_vulnapp/MainActivity.java
+++ app/src/main/java/positive/on/techtrain2018_sharefile_vulnapp/MainActivity.java
@@ -1,6 +1,9 @@
 package positive.on.techtrain2018_sharefile_vulnapp;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Environment;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -23,21 +26,26 @@ public class MainActivity extends AppCompatActivity {
         final TextView fileText = findViewById(R.id.fileText);
         Button shareButton = findViewById(R.id.shareButton);
 
+        final String packageName = this.getApplicationContext().getPackageName();
         shareButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 String text = fileText.getText().toString();
-                saveToFile(text);
-                Log.d("VulnApp","File has been written");
+                File file = saveToFile(text);
+
+                Uri uri = FileProvider.getUriForFile(getBaseContext(), packageName + ".fileprovider", file);
+                TextView contentUriView = findViewById(R.id.contentUri);
+                contentUriView.setText(uri.toString());
+                Log.d("FixedApp","File has been written");
             }
         });
 
 
     }
 
-    private void saveToFile(String text) {
+    private File saveToFile(String text) {
         String filename = "SharedFile.txt";
-        File file = new File(getExternalFilesDir(null), filename);
+        File file = new File(getFilesDir(), filename);
         FileOutputStream fos;
         byte[] data = text.getBytes();
         try {
@@ -52,5 +60,7 @@ public class MainActivity extends AppCompatActivity {
             Log.d("VulnApp","exception2");
             // handle exception
         }
+
+        return file;
     }
 }
